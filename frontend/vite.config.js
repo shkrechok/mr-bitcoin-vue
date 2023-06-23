ccc128625db89ac745d565792b3c9d810a076d42
Marina Milman <shkrechok@gmail.com>
2023-06-23 10:27:52 +0300

    First commit + dist

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/mister-bitcoin-vue',
+  base: '/mr-bitcoin-vue',
   plugins: [
     vue(),
   ],
